c42a8dc55d27f6c391a7d3e5d3fb93801f63b4c1
Andrea <nameigroupincIT@gmail.com>
2021-08-08 12:22:27 -0400

    for deploy

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,9 @@ const todayURL = `https://api.openweathermap.org/data/2.5/weather`
 export const fetchToday = async (zip_code) => {
   // Fetch weather data for today
 
-  const result = await axios.get(`${todayURL}?zip=${zip_code}&appid=${API_KEY}`)
+  const result = await axios.get(
+    `${todayURL}?zip=${zip_code}&appid=0578814adaa9e78432b3f4eed547a20b`
+  )
 
   return result.data
 }
